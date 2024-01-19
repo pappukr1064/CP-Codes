989b4ab1cabe08c48b80f6a6bda913d66aa069b2
pappukr1064 <96653525+pappukr1064@users.noreply.github.com>
2024-01-19 17:01:16 +0530

    Add files via upload

---
+++ PrimeF.cpp
@@ -0,0 +1,16 @@
+vector<long long> PrimeF(long long n) {
+    vector<long long> factorization;
+    while (n % 2 == 0) {
+        factorization.push_back(2);
+        n /= 2;
+    }
+    for (long long d = 3; d * d <= n; d += 2) {
+        while (n % d == 0) {
+            factorization.push_back(d);
+            n /= d;
+        }
+    }
+    if (n > 1)
+        factorization.push_back(n);
+    return factorization;
+}
